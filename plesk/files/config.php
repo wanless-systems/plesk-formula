8311cc52458cf1af81bc7eaf4c47bfe796b2c42d
WSL-Paul <62706545+WSL-Paul@users.noreply.github.com>
2020-03-31 11:24:11 +0100

    removed custom branding

--- plesk/files/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-
-# Custom Plesk Logo
-{%- if plesk.config.custom_logo is defined %}
-$config['title_image'] = "/usr/sbin/plesk bin branding --custom-logo -file /usr/local/psa/admin/cp/public/theme/images/logos/plesk/logo.png";
-{%- endif %}
